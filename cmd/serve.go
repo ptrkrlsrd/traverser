1a828ca012c42b2e30852882ab289e215d7beff4
Petter Karlsrud <petterkarlsrud@me.com>
2018-04-07 16:29:29 +0200

    Added a flag

--- cmd/serve.go
+++ cmd/serve.go
@@ -40,13 +40,5 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(serveCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	serveCmd.Flags().IntP("port", "p", 3000, "Port")
 }
