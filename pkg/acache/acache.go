699eeff2e232a739f092898a90e4bcc8652893ed
Petter Karlsrud <petterkarlsrud@me.com>
2018-04-07 16:06:01 +0200

    Initial

---
+++ pkg/acache/acache.go
@@ -0,0 +1,141 @@
+package acache
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/coreos/bolt"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	BoltBucketName = "acache"
+)
+
+type CacheItem struct {
+	ID    string `json:"key"`
+	URL   string `json:"url"`
+	Alias string `json:"alias"`
+	Data  []byte `json:"data"`
+}
+
+func CacheItemFromBytes(bytes []byte) (CacheItem, error) {
+	var cacheItem CacheItem
+	err := json.Unmarshal(bytes, &cacheItem)
+	if err != nil {
+		return cacheItem, err
+	}
+
+	return cacheItem, nil
+}
+
+type CacheStore struct {
+	DB *bolt.DB
+}
+
+func NewCache(db *bolt.DB) CacheStore {
+	cacheStore := CacheStore{DB: db}
+
+	return cacheStore
+}
+
+func (cacheStore *CacheStore) HasRoute(route string) bool {
+	return false
+}
+
+func (cacheStore CacheStore) HasItems() bool {
+	return true
+}
+
+func (cacheStore *CacheStore) ListRoutes() {
+	log.Println("list routes")
+	cacheItems, _ := cacheStore.GetCacheItems()
+	for _, v := range cacheItems {
+		fmt.Printf("%s %s\n", v.ID, v.Alias)
+	}
+}
+
+/*func (cacheStore CacheStore) GetURLS() []string {
+}*/
+
+func (cacheStore CacheStore) GetCacheItems() ([]CacheItem, error) {
+	var cacheItems []CacheItem
+
+	err := cacheStore.DB.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BoltBucketName))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			cacheItem, err := CacheItemFromBytes(v)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			cacheItems = append(cacheItems, cacheItem)
+		}
+
+		return nil
+	})
+
+	return cacheItems, err
+}
+
+func (cacheStore *CacheStore) AddRoute(url string, alias string) {
+	data := FetchJSON(url)
+	key := MD5Hash(alias)
+
+	cacheItem := CacheItem{ID: key, URL: url, Alias: alias, Data: data}
+	jsonData, err := json.Marshal(cacheItem)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cacheStore.DB.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BoltBucketName))
+		err := b.Put([]byte(key), jsonData)
+		return err
+	})
+}
+
+func FetchJSON(url string) []byte {
+	res, err := http.Get(url)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return body
+}
+
+func MD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func (cacheStore *CacheStore) StartServer(port string) {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	cacheItems, _ := cacheStore.GetCacheItems()
+
+	for _, v := range cacheItems {
+		router.GET(v.Alias, func(c *gin.Context) {
+			c.JSON(http.StatusOK, string(v.Data))
+		})
+	}
+
+	router.Run(":" + port)
+}
