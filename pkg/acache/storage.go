e0c917740525a3ac4e7f523255d863eb2d0ab4e0
Petter Karlsrud <ptrkrlsrd@users.noreply.github.com>
2019-02-03 18:16:51 +0100

    Update storage.go

--- pkg/acache/storage.go
+++ pkg/acache/storage.go
@@ -1,3 +1,17 @@
+// Copyright © 2019 Petter Karlsrud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package acache
 
 import (
@@ -27,6 +41,7 @@ func NewStorage(bucketName, path string, db *bolt.DB) (Storage, error) {
 	return Storage{BucketName: bucketName, DB: db}, nil
 }
 
+// Init Initializes the Bolt database
 func (storage *Storage) Init() error {
 	return storage.DB.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucket([]byte(storage.BucketName))
